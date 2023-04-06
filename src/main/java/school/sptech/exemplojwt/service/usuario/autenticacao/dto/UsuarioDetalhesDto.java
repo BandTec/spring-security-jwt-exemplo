d38e84c58b7da7a52006178bcea6cec72e7202a2
reisrb <rafael.reis@sptech.school>
2023-04-06 17:38:34 -0300

    Alterando retorno de false para true no userDetails

--- src/main/java/school/sptech/exemplojwt/service/usuario/autenticacao/dto/UsuarioDetalhesDto.java
+++ src/main/java/school/sptech/exemplojwt/service/usuario/autenticacao/dto/UsuarioDetalhesDto.java
@@ -41,21 +41,21 @@ public class UsuarioDetalhesDto implements UserDetails {
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return true;
   }
 }

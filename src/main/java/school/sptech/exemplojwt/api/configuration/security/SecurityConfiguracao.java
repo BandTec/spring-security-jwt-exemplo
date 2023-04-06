9c72c4edf1c0d0d80204a882a494859c1703ce4b
Diego Brito <diego.lima@sptech.school>
2023-04-06 14:05:41 -0300

    Correcao h2 console

--- src/main/java/school/sptech/exemplojwt/api/configuration/security/SecurityConfiguracao.java
+++ src/main/java/school/sptech/exemplojwt/api/configuration/security/SecurityConfiguracao.java
@@ -17,7 +17,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import school.sptech.exemplojwt.api.configuration.security.jwt.GerenciadorTokenJwt;
 import school.sptech.exemplojwt.service.usuario.autenticacao.AutenticacaoService;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -25,69 +24,71 @@ import java.util.List;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguracao {
 
-  @Autowired
-  private AutenticacaoEntryPoint autenticacaoJwtEntryPoint;
+    @Autowired
+    private AutenticacaoEntryPoint autenticacaoJwtEntryPoint;
 
-  @Autowired
-  private AutenticacaoService autenticacaoService;
+    @Autowired
+    private AutenticacaoService autenticacaoService;
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .cors()
-            .configurationSource(request-> {
-              CorsConfiguration configuration = new CorsConfiguration();
-              configuration.setAllowedOrigins(List.of("*"));
-              configuration.setAllowedMethods(List.of("*"));
-              configuration.setAllowedHeaders(List.of("*"));
-              return configuration;
-            })
-            .and()
-            .csrf()
-            .disable()
-            .authorizeRequests()
-            .antMatchers(
-                    "/usuarios/login"
-            ).permitAll()
-            .antMatchers().hasAnyRole()
-            .anyRequest().authenticated()
-            .and()
-            .exceptionHandling()
-            .authenticationEntryPoint(autenticacaoJwtEntryPoint)
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers()
+                .frameOptions().disable()
+                .and()
+                .cors()
+                .configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                })
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/usuarios/login", "/h2-console/**"
+                ).permitAll()
+                .antMatchers().hasAnyRole()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(autenticacaoJwtEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-    http.addFilterBefore(jwtAuthenticationFilterBean(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder =
-            http.getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder.authenticationProvider(new AutenticacaoProvider(autenticacaoService, passwordEncoder()));
-    return authenticationManagerBuilder.build();
-  }
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(new AutenticacaoProvider(autenticacaoService, passwordEncoder()));
+        return authenticationManagerBuilder.build();
+    }
 
-  @Bean
-  public AutenticacaoEntryPoint jwtAuthenticationEntryPointBean() {
-    return new AutenticacaoEntryPoint();
-  }
+    @Bean
+    public AutenticacaoEntryPoint jwtAuthenticationEntryPointBean() {
+        return new AutenticacaoEntryPoint();
+    }
 
-  @Bean
-  public AutenticacaoFilter jwtAuthenticationFilterBean() {
-    return new AutenticacaoFilter(autenticacaoService, jwtAuthenticationUtilBean());
-  }
+    @Bean
+    public AutenticacaoFilter jwtAuthenticationFilterBean() {
+        return new AutenticacaoFilter(autenticacaoService, jwtAuthenticationUtilBean());
+    }
 
-  @Bean
-  public GerenciadorTokenJwt jwtAuthenticationUtilBean() {
-    return new GerenciadorTokenJwt();
-  }
+    @Bean
+    public GerenciadorTokenJwt jwtAuthenticationUtilBean() {
+        return new GerenciadorTokenJwt();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
